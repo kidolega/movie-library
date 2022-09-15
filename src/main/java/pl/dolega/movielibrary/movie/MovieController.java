5bbdf51c91ad882be8259801052fadace32912a8
Marcin Dolega <marcin_dolega@epam.com>
2022-09-15 12:48:31 +0200

    2. CRUD getAll(), getById(), save() ; REST Mapping

---
+++ src/main/java/pl/dolega/movielibrary/movie/MovieController.java
@@ -0,0 +1,29 @@
+package pl.dolega.movielibrary.movie;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieController {
+
+    @Autowired
+    MovieRepository movieRepository;
+
+    @GetMapping("")
+    public List<Movie> getAll() {
+        return movieRepository.getAll();
+    }
+
+    @GetMapping("{id}")
+    public Movie getById(@PathVariable("id") int id) {
+        return movieRepository.getById(id);
+    }
+
+    @PostMapping("")
+    public int add(@RequestBody List<Movie> movies) {
+        return movieRepository.save(movies);
+    }
+}
---
+++ src/main/java/pl/dolega/movielibrary/movie/MovieRepository.java
@@ -0,0 +1,33 @@
+package pl.dolega.movielibrary.movie;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class MovieRepository {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public List<Movie> getAll() {
+        return jdbcTemplate.query("SELECT id, title, rating FROM movie",
+                BeanPropertyRowMapper.newInstance(Movie.class));
+    }
+
+    public Movie getById(int id) {
+        return jdbcTemplate.queryForObject("SELECT id, title, rating FROM movie WHERE id = ?",
+                BeanPropertyRowMapper.newInstance(Movie.class), id);
+    }
+
+    public int save(List<Movie> movies) {
+        movies.forEach(movie -> jdbcTemplate
+                        .update("INSERT INTO movie(title, rating) VALUES(?, ?)",
+                        movie.getTitle(), movie.getRating()
+                ));
+        return 1;
+    }
+}
